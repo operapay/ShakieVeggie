8fdcc32ceca38ec702e51e4f6b40b35d9dec9bfd
pay <opera.pay@gmail.com>
2018-10-01 20:32:11 +0700

    master start

---
+++ app.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const config = require('./config/database');
+
+mongoose.connect(config.database);
+let db = mongoose.connection;
+
+// check connection
+db.on('open',function(err){
+    console.log('connected to MongoDB');
+});
+
+// check for DB errors
+db.on('error',function(err){
+    console.log(err);
+});
+
+//Init App
+const app = express();
+
+// Load View Engine
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'pug');
+
+//body paeser middleware
+//parse opplication/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse opplication/json
+app.use(bodyParser.json())
+
+// set public folder
+app.use(express.static(path.join(__dirname,'public')));
+
+// Express Session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
+
+// Express message middleware
+app.use(flash());
+app.use(function(req,res,next){
+    res.locals.messages = require('express-messages')(req,res);
+    next();
+});
+
+// Express validator
+app.use(expressValidator({
+    errorFormatter: function(param,msg,value){
+        var namespace = param.split('.')
+        ,root = namespace.shift()
+        ,formParam = root;
+
+        while(namespace.length){
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return{
+            param : formParam,
+            msg : msg,
+            value : value
+        };
+    }
+}));
+
+//passport config
+require('./config/passport')(passport);
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('*',function(req,res,next){
+    res.locals.user = req.user || null;
+    next();
+});
+
+// Home Route
+app.get('/', function(req,res){
+    res.render('index', {
+        //title:'Medals Table' ,
+        //articles: articles
+    });
+});
+
+let users = require('./routes/users');
+app.use('/users', users);
+
+// Start Server
+app.listen(3000, function(){
+    console.log('Server started on port 3000...');
+});
---
+++ routes/users.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const db = require('../config/database');
+
+//Bring in User model
+let User = require('../models/user');
+
+//Register form
+router.get('/register', function(req,res){
+    res.render('register');
+});
+
+//register process
+router.post('/register', function(req,res,done){
+    const firstname = req.body.firstname;
+    const lastname = req.body.lastname;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const username = req.body.username;
+    const password = req.body.password;
+    const password2 = req.body.password2;
+
+    req.checkBody('firstname','Firstname is required').notEmpty();
+    req.checkBody('lastname','Lastname is required').notEmpty();
+    req.checkBody('email','Email is required').notEmpty();
+    req.checkBody('email','Email is not valid').isEmail();
+    req.checkBody('phone','Phone is required').notEmpty();
+    req.checkBody('username','Username is required').notEmpty();
+    req.checkBody('password','Password is required').notEmpty();
+    req.checkBody('password2','Password do not match').equals(req.body.password);
+
+
+    let errors = req.validationErrors();
+    //let check = req.username();
+
+    if(errors){
+        res.render('register',{
+            errors:errors
+        });
+    }
+    else{
+        let newUser = new User({
+            firstname:firstname,
+            lastname:lastname,
+            email:email,
+            phone:phone,
+            username:username,
+            password:password
+        });
+
+        User.findOne({
+            username: username
+        }, function(err,user) {
+            if (user) {
+                req.flash('danger','Username Already Exists');
+                res.redirect('/users/register');
+            }
+            else{
+                bcrypt.genSalt(10, function(err,salt){
+                    bcrypt.hash(newUser.password,salt,function(err,hash){
+                        if(err){
+                            console.log(err);
+                        }
+                        newUser.password = hash;
+                        newUser.save(function(err){
+                            if(err){
+                                console.log(err);
+                                return;
+                            }
+                            else{
+                                req.flash('success','You are now registered and can login');
+                                res.redirect('/users/login');
+                            }
+                        });
+                    });
+                });
+            }
+        });
+    }
+    
+});
+
+//login form
+router.get('/login', function(req,res){
+    res.render('login');
+});
+
+//login process
+router.post('/login', function(req,res,next){
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req,res,next);
+});
+
+//logout
+router.get('/logout',function(req,res){
+    req.logout();
+    req.flash('success','You are logged out');
+    res.redirect('/users/login');
+});
+
+module.exports = router;
