e2b7abc298305e2ddf8e93e313be9719581b00ca
nchp <nicha.lim@ku.th>
2018-10-17 12:03:06 +0700

    .

--- routes/recommends.js
+++ routes/recommends.js
@@ -2,13 +2,92 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/database');
 
-//Bring in Recommend Model
-let Recommend = require('../models/recommend'); 
 
-router.get('/3', function(req,res){
-    Recommend.findOne({bottle:3}, function(err, bottle){
-        res.render('bottle_3', {bottle: bottle});
-    })
-})
+let Formula = require('../models/formula');
+let Nutrient = require('../models/nutrient');
+
+
+router.get('/:id', function(req,res){
+    Formula.findById(req.params.id, function(err,formula){
+        Nutrient.findById(formula.component1,function(err,nutrient1){
+            Nutrient.findById(formula.component2,function(err,nutrient2){
+                Nutrient.findById(formula.component3,function(err,nutrient3){
+                    res.render('custom', {
+                        formula:formula,
+                        component1:nutrient1,
+                        component2:nutrient2,
+                        component3:nutrient3
+                    }); 
+                });
+            });
+        });
+    });
+
+    checkkidney = 0;
+    checkliver = 0;
+    checkhypertension = 0;
+    checkdiabetes = 0;
+    checkpregnant = 0;
+    checksurgery = 0;
+});
+
+router.post('/:id', function (req, res) {
+    //res.json(req.body);
+    const kidney = req.body.kidney;
+    const liver = req.body.liver;
+    const hypertension = req.body.hypertension;
+    const diabetes = req.body.diabetes;
+    const pregnant = req.body.pregnant;
+    const surgery = req.body.surgery;
+    if(kidney){
+        checkkidney = 1;
+        console.log('kidney');
+    }
+    if(liver){
+        checkliver = 1;
+        console.log('liver');
+    }
+    if(hypertension){
+        checkhypertension = 1;
+        console.log('hypertension');
+    }
+    if(diabetes){
+        checkdiabetes = 1;
+        console.log('diabetes');
+    }
+    if(pregnant){
+        checkpregnant = 1;
+        console.log('pregnant');
+    }
+    if(surgery){
+        checksurgery = 1;
+        console.log('surgery');
+    }
+    Formula.findById(req.params.id, function(err,formula){
+        res.redirect('/custom/'+ formula._id + '/component');
+    });
+});
+
+router.get('/:id/component',function(req,res){
+    Formula.findById(req.params.id, function(err,formula){
+        Nutrient.findById(formula.component1,function(err,nutrient1){
+            Nutrient.findById(formula.component2,function(err,nutrient2){
+                Nutrient.findById(formula.component3,function(err,nutrient3){
+                    res.render('customnodisease', {
+                        formula:formula,
+                        component1:nutrient1,
+                        component2:nutrient2,
+                        component3:nutrient3
+                    }); 
+                });
+            });
+        });
+    });
+});
+
+router.post('/:id/component',function(req,res){
+    req.flash('success','Added order to cart');
+    res.redirect('/formulas');
+});
 
 module.exports = router;
