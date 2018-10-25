ed8fb0d76e0e995a4a33817984050473c2cb617f
nchp <nicha.lim@ku.th>
2018-10-25 19:25:36 +0700

    cart

--- routes/cart.js
+++ routes/cart.js
@@ -4,16 +4,19 @@ const db = require('../config/database');
 
 let Bottle = require('../models/bottle');
 let Order = require('../models/order');
+let Formula = require('../models/formula');
 
 router.get('/:order',function(req,res){
-
-    console.log(req.params.order);
-    
-    Order.findById(req.params.order,function(err,orders){
-        res.render('cart', {
-            order:orders._id,
-            ordetail:orders
-        });
+    Bottle.find({},function(err,bottle){
+        Order.findById(req.params.order,function(err,order){
+            Formula.findById(bottle.formulaid,function(err,formula){
+                res.render('cart', {
+                    order:order._id,
+                    bottle:bottle,
+                    formula:formula.name
+                });
+            });
+        })
     });
 });
 
