822d778fe7b5948dc335a16a86382ba76a3fc889
pay <opera.pay@gmail.com>
2018-10-26 19:13:26 +0700

    delete

--- routes/cart.js
+++ routes/cart.js
@@ -20,4 +20,26 @@ router.get('/:id',function(req,res){
     });
 });
 
+router.post('/:id/:bottle',function(req,res){
+    //console.log(req.params.id);
+    let query = {_id:req.params.bottle}
+
+    Bottle.remove(query, function(err){
+        if(err){
+           console.log(err); 
+        }
+        Order.findById(req.params.id, function(err,orders){
+            Bottle.find({},function(err,bottles){
+                Formula.find({},function(err,formulas){
+                    res.render('cart',{
+                        bottles:bottles,
+                        orders:orders,
+                        formulas:formulas
+                    });
+                });
+            });
+        });
+    });
+});
+
 module.exports = router;
